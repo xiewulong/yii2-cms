5dbd6e76c2bdfb1d3c5dfb0e59fd3b72a1524842
xiewulong <xiewulong@vip.qq.com>
2016-09-20 16:19:57 +0800

    cms

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,22 +4,21 @@
  * create: 2016/9/18
  * since: 0.0.1
  */
-const fs = require('fs');
-const path = require('path');
+const PATH = require('path');
 
-const gulp = require('gulp');
-const plugins = require('gulp-load-plugins')();
+const GULP = require('gulp');
+const PLUGINS = require('gulp-load-plugins')();
 
-const basePath = __dirname;
-const config = {
+const BASE_PATH = __dirname;
+const CONFIG = {
 	css: {
-		src: path.join(basePath, 'scss', '*.scss'),
-		dist: path.join(basePath, 'dist', 'css'),
+		src: PATH.join(BASE_PATH, 'scss', '*.scss'),
+		dist: PATH.join(BASE_PATH, 'dist', 'css'),
 		outputStyles: ['compact', 'compressed'],
 	},
 	js: {
-		src: path.join(basePath, 'js'),
-		dist: path.join(basePath, 'dist', 'js'),
+		src: PATH.join(BASE_PATH, 'js'),
+		dist: PATH.join(BASE_PATH, 'dist', 'js'),
 		concat: {
 			'backend.js': {
 				header: {
@@ -52,36 +51,36 @@ const config = {
 let ns = 'cms';
 let _ns = (name, parent = ns) => (parent ? parent + ':' : '') + name;
 
-gulp.task(_ns('scss'), function() {
-	config.css.outputStyles.forEach((style) => {
-		gulp.src(config.css.src)
-			.pipe(plugins.sourcemaps.init())
-			.pipe(plugins.sass({outputStyle: style}).on('error', plugins.sass.logError))
-			.pipe(plugins.rename(function(path) {
+GULP.task(_ns('scss'), function() {
+	CONFIG.css.outputStyles.forEach((style) => {
+		GULP.src(CONFIG.css.src)
+			.pipe(PLUGINS.sourcemaps.init())
+			.pipe(PLUGINS.sass({outputStyle: style}).on('error', PLUGINS.sass.logError))
+			.pipe(PLUGINS.rename(function(path) {
 				if(style == 'compressed') {
 					path.basename += '.min';
 				}
 			}))
-			.pipe(plugins.sourcemaps.write('.'))
-			.pipe(gulp.dest(config.css.dist));
+			.pipe(PLUGINS.sourcemaps.write('.'))
+			.pipe(GULP.dest(CONFIG.css.dist));
 	});
 });
 
 let jsTasks = [];
 let jsWatchConfigs = [];
-for(let distName of Object.keys(config.js.concat)) {
-	let concatConfig = config.js.concat[distName];
+for(let distName of Object.keys(CONFIG.js.concat)) {
+	let concatConfig = CONFIG.js.concat[distName];
 	let jsTask = _ns('babel:concat:' + distName);
 	let globs = [];
 	concatConfig.deps.forEach((file) => {
-		globs.push(path.join(config.js.src, file));
+		globs.push(PATH.join(CONFIG.js.src, file));
 	});
-	gulp.task(jsTask, function() {
-		gulp.src(globs)
-			.pipe(plugins.sourcemaps.init())
-			.pipe(plugins.concat(distName))
-			.pipe(plugins.babel())
-			.pipe(plugins.header([
+	GULP.task(jsTask, function() {
+		GULP.src(globs)
+			.pipe(PLUGINS.sourcemaps.init())
+			.pipe(PLUGINS.concat(distName))
+			.pipe(PLUGINS.babel())
+			.pipe(PLUGINS.header([
 				'/*!',
 				' * ' + distName.replace('.js', ''),
 				' * ' + concatConfig.header.author + ' <' + concatConfig.header.email + '>',
@@ -89,14 +88,14 @@ for(let distName of Object.keys(config.js.concat)) {
 				' * since: ' + concatConfig.header.since,
 				' */',
 			].join('\n') + '\n'))
-			.pipe(plugins.sourcemaps.write('.'))
-			.pipe(gulp.dest(config.js.dist));
-		gulp.src(globs)
-			.pipe(plugins.sourcemaps.init())
-			.pipe(plugins.concat(distName))
-			.pipe(plugins.babel())
-			.pipe(plugins.uglify())
-			.pipe(plugins.header([
+			.pipe(PLUGINS.sourcemaps.write('.'))
+			.pipe(GULP.dest(CONFIG.js.dist));
+		GULP.src(globs)
+			.pipe(PLUGINS.sourcemaps.init())
+			.pipe(PLUGINS.concat(distName))
+			.pipe(PLUGINS.babel())
+			.pipe(PLUGINS.uglify())
+			.pipe(PLUGINS.header([
 				'/*!',
 				' * ' + distName.replace('.js', ' minify'),
 				' * ' + concatConfig.header.author + ' <' + concatConfig.header.email + '>',
@@ -104,11 +103,11 @@ for(let distName of Object.keys(config.js.concat)) {
 				' * since: ' + concatConfig.header.since,
 				' */',
 			].join('\n') + '\n'))
-			.pipe(plugins.rename(function(path) {
+			.pipe(PLUGINS.rename(function(path) {
 				path.basename += '.min';
 			}))
-			.pipe(plugins.sourcemaps.write('.'))
-			.pipe(gulp.dest(config.js.dist));
+			.pipe(PLUGINS.sourcemaps.write('.'))
+			.pipe(GULP.dest(CONFIG.js.dist));
 	});
 	jsTasks.push(jsTask);
 	jsWatchConfigs.push({
@@ -117,12 +116,12 @@ for(let distName of Object.keys(config.js.concat)) {
 	});
 }
 
-gulp.task(_ns('watch'), function() {
-	gulp.watch(config.css.src, [_ns('scss')]);
+GULP.task(_ns('watch'), function() {
+	GULP.watch(CONFIG.css.src, [_ns('scss')]);
 	jsWatchConfigs.forEach(({glob, tasks}) => {
-		gulp.watch(glob, tasks);
+		GULP.watch(glob, tasks);
 	});
 });
 
-gulp.task('release', (gulp.tasks.release ? gulp.tasks.release.dep : []).concat([_ns('scss')], jsTasks));
-gulp.task('default', (gulp.tasks.default ? gulp.tasks.default.dep : []).concat([_ns('watch')]));
+GULP.task('release', (GULP.tasks.release ? GULP.tasks.release.dep : []).concat([_ns('scss')], jsTasks));
+GULP.task('default', (GULP.tasks.default ? GULP.tasks.default.dep : []).concat([_ns('watch')]));
